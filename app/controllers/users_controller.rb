65f3fb47040d2ac7f739bb8e5320f8144b540480
kartik-biz4 <kartik@biz4solutions.com>
2015-11-28 18:07:00 +0530

    Added validations to signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController
+  
   def new
-    @user = User.new
+    @user = User.new    
   end
 
   def show
     user_id = params[:id]
     @user = User.find(user_id)
   end
-  
+
+  def create
+    user_check = User.find_by_email(params[:email])
+    if user_check.present?
+      flash[:danger] = "User already exists!"
+    end
+    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar))
+    Rails.logger.info(@user.errors.inspect) 
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
 end
